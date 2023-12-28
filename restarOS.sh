b6408da9207dde4b4f0db11722a4d0adb95c3e69
Jonnathan Clay <elclay7@users.noreply.github.com>
2023-12-28 14:37:14 -0300

    Update restarOS.sh

--- restarOS.sh
+++ restarOS.sh
@@ -1,30 +1,37 @@
 #!/bin/bash
+
 logfile="/var/log/restarOS.log"
-fecha_hora=$(date +"%d-%m-%Y %H:%M:%S")
-echo "$fecha_hora Inicio del script restarOS.sh" >> "$logfile"
+
+obtener_fecha_hora() {
+  date +"%d-%m-%Y %H:%M:%S"
+}
+
+echo "$(obtener_fecha_hora) Inicia script /root/restarOS.sh" >> "$logfile"
+
+# Inicialización de variables
 tiempo=0
 
+# Bucle principal
 while true; do
+  if ! ping -c 1 cl.pool.ntp.org > /dev/null; then
+    fecha_hora_ping=$(obtener_fecha_hora)
+    echo "$fecha_hora_ping Ping fallido" >> "$logfile"
+    
+    # Incrementa el tiempo acumulado en 10 segundos
+    tiempo=$((tiempo + 10))
+    
+    # Si el tiempo acumulado supera los 120 segundos, reinicia el sistema operativo
+    if [ $tiempo -ge 120 ]; then
+      fecha_hora_evento=$(obtener_fecha_hora)
+      echo "$fecha_hora_evento El ping a cl.pool.ntp.org no respondió durante 2 minuto" >> "$logfile"
+      echo "$fecha_hora_evento Reiniciando el sistema operativo" >> "$logfile"
+      reboot
+    fi
+  else
+    # Si el ping responde, reinicia el contador de tiempo acumulado
+    tiempo=0
+  fi
 
-  ping -c 1 cl.pool.ntp.org > /dev/null
-
-  # Si el ping no respondió, incrementa el tiempo acumulado
-  if [ $? -ne 0 ]; then
-    tiempo=$((tiempo + 10)) # Incrementa la variable de tiempo en 10 segundos
-  else
-    # Si el ping responde, reinicia el contador de tiempo acumulado
-    tiempo=0
-  fi
-
-  # Si el tiempo acumulado supera los 300 segundos (5 minutos), reinicia el sistema operativo
-  if [ $tiempo -ge 300 ]; then
-    fecha_hora_evento=$(date +"%d-%m-%Y %H:%M:%S")
-    echo "$fecha_hora_evento El ping a cl.pool.ntp.org no respondió durante 5 minutos" >> "$logfile"
-    echo "$fecha_hora_evento Reiniciando el sistema operativo" >> "$logfile"
-    reboot
-  fi
-
-  sleep 10
+  sleep 10
 
 done
-
